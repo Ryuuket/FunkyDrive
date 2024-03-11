3ea446a793b4fc50d099cfcfa647aa006c73931c
Armaiic <bastiendandre.pro@gmail.com>
2024-03-11 11:06:06 +0100

    change route homepage

--- src/main/kotlin/com/example/plugins/Templating.kt
+++ src/main/kotlin/com/example/plugins/Templating.kt
@@ -1,6 +1,7 @@
 package com.example.plugins
 
 import freemarker.cache.*
+import io.ktor.client.engine.*
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
 import io.ktor.server.response.*
@@ -14,7 +15,12 @@ fun Application.configureTemplating() {
         get("/html-freemarker") {
             call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
         }
+        get("/home") {
+            val data = mapOf("message" to "Welcome to Funky-drive home page!")
+            call.respond(FreeMarkerContent("home.ftl",data, ""))
+        }
     }
 }
 
 data class IndexData(val items: List<Int>)
+
